375663eec3da4eb2bf9bb548139a0dd2f24b8aa4
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-09-09 17:28:46 +0530

    Add functions for enhanced modularity

--- app/CPPinator/src/CPPinator.py
+++ app/CPPinator/src/CPPinator.py
@@ -3,6 +3,38 @@ import subprocess
 import time
 
 
+def _displayCPPFiles(cpp_files):
+    """Helper function to display the list of .cpp files in the specified directory.
+
+    Args:
+        cpp_files (list): List of .cpp files in the directory.
+    """
+
+    print("List of .cpp files:\n")
+    for i, cpp_file in enumerate(cpp_files):
+        print(f"{i+1}) \033[94m{cpp_file}\033[0m")
+
+
+def _displayFinalOutput(success, total_time, problem_files):
+    """Helper function to display the final output after running all the .cpp files.
+
+    Args:
+        success (bool): Flag to check if all the .cpp files ran successfully.
+        total_time (float): Total time taken to compile & run all the .cpp files.
+        problem_files (list): List of .cpp files that failed to compile/run.
+    """
+
+    if success:
+        print(
+            f"✅ \033[92mRan all C++ files successfully.\033[0m \033[90m({total_time}s)\033[0m")
+    else:
+        print(
+            f"❌ \033[91mSome C++ files failed to compile/run.\033[0m \033[90m({total_time}s)\033[0m")
+        print("Problem files:")
+        for i, problem_file in enumerate(problem_files):
+            print(f"{i+1}) \033[94m{problem_file}\033[0m")
+
+
 def compile_and_run_cpp_files(directory_path):
     """Compile and run all the .cpp files in the specified directory. The compiled executable is deleted after running the program. The time taken to compile and run each .cpp file is printed, along with the output of the program. If any .cpp file fails to compile or run, the error message is printed, and the total time taken is also printed. The function assumes that the C++ compiler (g++) is installed and added to the system PATH.
 
@@ -10,7 +42,7 @@ def compile_and_run_cpp_files(directory_path):
         directory_path (str): The path of the directory containing the .cpp files.
     """
 
-    a_exe = 'a.exe'  # Name of the compiled executable
+    a_exe = "a.exe"  # Name of the compiled executable
 
     # Change the current working directory to the specified path
     print("Changing working directory...")
@@ -26,9 +58,7 @@ def compile_and_run_cpp_files(directory_path):
     if not cpp_files:
         print("No .cpp files found in the directory.")
         return
-    print("List of .cpp files:\n")
-    for i, cpp_file in enumerate(cpp_files):
-        print(f"{i+1}) \033[94m{cpp_file}\033[0m")
+    _displayCPPFiles(cpp_files)
 
     total_time = 0  # Total time taken to compile & run all the .cpp files
 
@@ -38,6 +68,7 @@ def compile_and_run_cpp_files(directory_path):
     problem_files = []  # List of .cpp files that failed to compile/run
     for i, cpp_file in enumerate(cpp_files):
 
+        # Start the timer
         start_time = time.time()
 
         # Compile the C++ file
@@ -62,6 +93,7 @@ def compile_and_run_cpp_files(directory_path):
             )
             run_output, run_errors = run_process.communicate()
 
+            # End the timer & calculate the time taken
             end_time = time.time()
             time_s = round((end_time - start_time), 2)
             total_time += time_s
@@ -85,19 +117,12 @@ def compile_and_run_cpp_files(directory_path):
     # Delete the compiled executable
     os.remove(a_exe)
 
+    # Round off the total time to 2 decimal places
     total_time = round(total_time, 2)
 
-    if success:
-        print(
-            f"✅ \033[92mRan all C++ files successfully.\033[0m \033[90m({total_time}s)\033[0m")
-    else:
-        print(
-            f"❌ \033[91mSome C++ files failed to compile/run.\033[0m \033[90m({total_time}s)\033[0m")
-        print("Problem files:")
-        for i, problem_file in enumerate(problem_files):
-            print(f"{i+1}) \033[94m{problem_file}\033[0m")
+    # Display the final output
+    _displayFinalOutput(success, total_time, problem_files)
 
 
 if __name__ == "__main__":
-
     compile_and_run_cpp_files(directory_path='./Basic Problems/')
